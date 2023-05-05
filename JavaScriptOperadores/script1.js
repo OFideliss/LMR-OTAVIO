d38bd9d7ebdb1a5a31a90e18ac8a0ec3f3b05848
OFideliss <otavio-fidelis@outlook.com>
2023-05-04 21:51:07 -0300

    04/05/2023

---
+++ JavaScriptOperadores/script1.js
@@ -0,0 +1,11 @@
+//Script de valor inteiro
+let numero1;
+let numero2;
+let resposta;
+
+numero1 = prompt('Digite um valor:' , 'Digite um número:');
+numero2 = prompt('Digite outro valor:' , 'Digite um número:');
+//realiza a soma de dois n° inteiros
+resposta = parseInt(numero1) + parseInt(numero2);
+
+document.write('O resultado da soma é: ' , resposta);
