f45b8ffdfa9862d22225aadf1a14bae471ffc22a
OFideliss <otavio-fidelis@outlook.com>
2023-05-12 09:14:12 -0300

    12/05/23

--- JavaScriptFormulario/script.js
+++ JavaScriptFormulario/script.js
@@ -1,15 +1,46 @@
 function Concatenar() {
-    let Nome = document.getElementById("pNome").value;
-    let Sobrenome = document.getElementById("pSobrenome").value;
-    let Concatenado = Nome + ' ' + Sobrenome;
-    alert('Olá ' + Concatenado);
+    let nome = document.getElementById("pNome").value;
+    let sobrenome = document.getElementById("pSobrenome").value;
+    let concatenado = nome + ' ' + sobrenome;
+    alert('Olá ' + concatenado);
 }
 
-function ola() {
-    let Numero1 = document.getElementById("Numero1").value;
-    let Numero2 = document.getElementById("Numero2").value;
-    let Operacao = document.getElementById("teste").value;
-    let Resultado;
-    if(Operacao==Soma) {
-        if(Operacao==Soma) {
-            if(Operacao==Soma) {
+function Soma() {
+    let numero1 = document.getElementById("Numero1").value;
+    let numero2 = document.getElementById("Numero2").value;
+    let resultado = parseInt(numero1) + parseInt(numero2);
+
+    alert("O resultado da soma é: " + resultado);
+
+}
+
+function Subtracao() {
+    let numero1 = document.getElementById("Numero1").value;
+    let numero2 = document.getElementById("Numero2").value;
+    let resultado = parseFloat(numero1) - parseFloat(numero2);
+
+    alert("O resultado da subtração é: " + resultado);
+
+}
+
+function Multiplicacao() {
+    let numero1 = document.getElementById("Numero1").value;
+    let numero2 = document.getElementById("Numero2").value;
+    let resultado = parseFloat(numero1) * parseFloat(numero2);
+
+    alert("O resultado da Multiplicação é: " + resultado);
+
+}
+
+function Divisao() {
+    let numero1 = document.getElementById("Numero1").value;
+    let numero2 = document.getElementById("Numero2").value;
+
+    if (numero2 == 0) {
+        alert("Não existe divisão por 0");
+    } else {
+        let resultado = parseFloat(numero1) / parseFloat(numero2);
+        alert("O resultado da Divisão é: " + resultado);
+    }
+
+}
