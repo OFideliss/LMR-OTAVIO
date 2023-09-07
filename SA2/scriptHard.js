af01906339d63dd4025646cb2f57b8a2c064e9fe
OFideliss <otavio-fidelis@outlook.com>
2023-09-06 21:49:39 -0300

    06/09/23

--- SA2/scriptHard.js
+++ SA2/scriptHard.js
@@ -19,14 +19,20 @@ function verificarSelecao() {
     }
 }
 
+//=============Seleção do Usuario=============================================
 function Selecao(opcao) {
     escolhaUsuario = opcao;
+    const imagemUsuario = document.getElementById('imagem1');
     // alert('Escolha do usuário: ' + escolhaUsuario);
     selecaoTexto.textContent = `Você selecionou: ${opcao}`;
     verificarSelecao(); // Verifica a seleção sempre que uma opção é escolhida
     opcaoSelecionada = opcao;
+    // Atualiza a imagem do usuário com base na escolha
+    imagemUsuario.src = imagensSelecao[opcao];
+
 }
 
+//=============Confirmar Seleão do Usuario==================================== 
 function confirmarEscolha() {
     //Armazena localmente o valor de opcaoSelcionada
     localStorage.setItem('opcaoSelecionada', opcaoSelecionada);
@@ -34,8 +40,9 @@ function confirmarEscolha() {
     window.location.href = ResultadoURL;
 }
 
-//==========================================Resultado============================================
-//Sorteia a imagem
+//===========================================================Resultado=============================================================
+
+//============Sorteia a imagem do computador==================================
 function sortearImagem() {
     const imagens = ["img/PedraSemFundo.png", "img/PapelSemFundo.png", "img/TesouraSemFundo.png"];
     // Gera um número aleatório entre 0 e 2 (a multiplicação pelo comprimento do vetor imagens quer dizer que ira sortear um número entre 0 e 2)
@@ -48,25 +55,23 @@ function sortearImagem() {
 
     // Define o caminho da imagem com base no número aleatório gerado
     minhaImagemSegundoCard.src = imagens[numeroAleatorio];
-
-
-
-
-    
 }
 
+//============Mostra a Section com o resultado==================================
 function mostrarResultado() {
     var mostraResultado = document.getElementById("Resultado");
     var mostraGame = document.getElementById("Game");
-
+    var botaoSelecao = document.getElementById("botaoSelecao");
+    
     if (mostraResultado.style.display === "none") {
         mostraGame.style.display = "none";
+        botaoSelecao.style.display = "none";
         mostraResultado.style.display = "block";
     } else {
         mostraResultado.style.display = "none";
     }
 }
 
-// Chame a função para sortear a imagem aleatória quando necessário
+// Chame a função para sortear a imagem aleatória
 sortearImagem();
 
